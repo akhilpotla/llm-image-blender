332fcc5bf1bf18d894590343ed6be42bdb68f82b
Akhil Potla <akhil4400@gmail.com>
2025-01-18 10:10:47 -0700

    UUID for each request

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
@@ -17,13 +18,18 @@ app.use(
   })
 );
 
+// Middleware to add a UUID to each request
+app.use((req, res, next) => {
+  req.id = uuidv4();
+  console.log(`Request ID: ${req.id}`);
+  next();
+});
+
 app.get("/", (req, res) => res.send("API Running"));
 
 app.use("/api/v1/images", require("./routes/api/images"));
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
