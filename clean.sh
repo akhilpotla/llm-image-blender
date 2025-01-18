47b51b2a96c4c009fe1c68c47e3e1bfce74f75f4
Akhil Potla <akhil4400@gmail.com>
2025-01-18 14:55:10 -0700

    Fixed docker compose filename

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-docker compose -f docker_compose.yml down
+docker compose -f docker-compose.yml down
 
 docker rm $(docker ps -a -f status=exited -q)
 docker rmi $(docker images -a)
