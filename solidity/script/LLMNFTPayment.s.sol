f6ae9cde3e14f6d0181fbfa23eaeb87d9e307740
Akhil Potla <akhil4400@gmail.com>
2025-01-27 19:12:41 -0700

    Renaming Counter scripts and tests

--- solidity/script/LLMNFTPayment.s.sol
+++ solidity/script/LLMNFTPayment.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {LLMNFTPayment} from "../src/LLMNFTPayment.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract LLMNFTPaymentScript is Script {
+    LLMNFTPayment public llmNFTPayment;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        llmNFTPayment = new LLMNFTPayment();
 
         vm.stopBroadcast();
     }
